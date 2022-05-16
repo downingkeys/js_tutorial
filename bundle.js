9e8f15250aae27928057b11cb7317286e84f3782
Timothy Downing <downingkeys@gmail.com>
2022-05-16 16:46:22 -0700

    Finally got it to bump to version 0.2.0

--- bundle.js
+++ bundle.js
@@ -47,7 +47,11 @@ function Phrase(content) {
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.processedContent()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
 }
 
